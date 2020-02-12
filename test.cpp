80489eb8f284c7a48403a956d17446c9c40ddfda
Samuel Razumovskiy <camka14@gmail.com>
2020-02-12 16:48:33 -0700

    Added test case for SubtractN

--- test.cpp
+++ test.cpp
@@ -8,8 +8,16 @@ TEST_CASE("Negative returns -1 and positive returns 1", "[Sign]"){
   REQUIRE(Sign(-1.0) == -1.0);
 }
 
+<<<<<<< Updated upstream
 std::vector<int> sum_vector = {1,2,3};
 
 TEST_CASE("Sum adds up all the numbers in a vector", "[Sum]"){
   REQUIRE(Sum(sum_vector) == 6);
 }
+=======
+TEST_CASE("Returns new vector with n subtracted from vector for ints","[SubtractN]"){
+  std::vector<int> vec = {0,1,3};
+  std::vector<int> vec2 = {-3,-2,0};
+  REQUIRE(SubtractN(vec,3) == vec2);
+}
+>>>>>>> Stashed changes
