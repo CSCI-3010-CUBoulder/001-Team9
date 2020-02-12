2e6a716b3d919db15450a8b3d75b4c38a6c60dea
Travis024 <travis.torline@colorado.edu>
2020-02-12 16:37:12 -0700

    Updated makefile to remove functions_to_implement

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "functions_to_implement.cpp"
 
 TEST_CASE("Negative returns -1 and positive returns 1", "[Sign]"){
   REQUIRE(Sign(5.0) == 1.0);
