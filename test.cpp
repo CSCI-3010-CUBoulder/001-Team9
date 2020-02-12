56019fbc4596720715f76f918f210df0c7444728
Meigs2 <ConnorRadeloff1@gmail.com>
2020-02-12 16:57:56 -0700

    Added more test cases

--- test.cpp
+++ test.cpp
@@ -19,3 +19,11 @@ TEST_CASE("Returns new vector with n subtracted from vector for ints","[Subtract
   std::vector<int> vec2 = {-3,-2,0};
   REQUIRE(SubtractN(vec,3) == vec2);
 }
+
+TEST_CASE("Subtracting 1.0 and -1.0 from vectors works properly.", "[SubtractN]"){
+  std::vector<double> num{1.0,2.0,3.0};
+  std::vector<double> ans1{0.0,1.0,2.0};
+  std::vector<double> ans2{2.0,3.0,4.0};
+  REQUIRE(SubtractN(num, 1.0) == ans1);
+  REQUIRE(SubtractN(num, -1.0) == ans2);
+}
