2480edee8fb2edc82fce38a315207a739e0b1472
Travis024 <travis.torline@colorado.edu>
2020-02-12 16:47:51 -0700

    test case for sum function

--- test.cpp
+++ test.cpp
@@ -7,3 +7,9 @@ TEST_CASE("Negative returns -1 and positive returns 1", "[Sign]"){
   REQUIRE(Sign(0.0) == 1.0);
   REQUIRE(Sign(-1.0) == -1.0);
 }
+
+std::vector<int> sum_vector = {1,2,3};
+
+TEST_CASE("Sum adds up all the numbers in a vector", "[Sum]"){
+  REQUIRE(Sum(sum_vector) == 6);
+}
